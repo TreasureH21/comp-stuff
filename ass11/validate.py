c478e251b50093b1e5ec24b1f2773aab7e9e770b
TreasureH21 <baladityavs@gmail.com>
2024-10-19 11:30:49 +0530

    Update validate.py

--- ass11/validate.py
+++ ass11/validate.py
@@ -4,34 +4,30 @@ def otp():
     otp = random.randint(1000,9999)
     return otp
 
+
+
 def captcha():
+    def rcs(x): # rcs for random character selector
+        return (x[random.randint(0,len(x)-1)])
+    
     length = int(input("Enter length of captcha: "))
     while length <4:
         length = int(input("Re-enter captcha length (atleast 4): "))
-    sc = ["!","#", "$", "%", "&", "*", "?", "@", "^", "_"]
-    uc = chr(random.randint(65,90))
-    lc = chr(random.randint(97,122))
-    digits = (random.randint(0,9))
+    sc = "!#$%&*?@^_"
+    uc = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    lc = "qwertyuiopasdfghjklzxcvbnm"
+    digits = "1234567890"
 
     l = []
-    l += [uc,sc[random.randint(0,len(sc)-1)],lc,str(digits)]
+    l += [rcs(sc)] + [rcs(uc)] + [rcs(lc)] + [rcs(digits)]
     (random.shuffle(l))
     s = ''
     for i in l:
         s += i
     k = 4
+    total = sc + uc + lc + digits
     while k<=length:
-        l = []
-        l += [uc,sc[random.randint(0,len(sc)-1)],lc,str(digits)]
-        (random.shuffle(l))
-        s += l[0]
+        s += rcs(total)
         k += 1
 
     return s
-
-print(otp())
-print(captcha())
-
-
-    
-
