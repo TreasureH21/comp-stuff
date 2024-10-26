19ed22580bccc42e3289c87dfd4e43a7dc7e2187
TreasureH21 <baladityavs@gmail.com>
2024-10-26 11:19:25 +0530

    Update validate.py

--- ass11/validate.py
+++ ass11/validate.py
@@ -4,30 +4,33 @@ def otp():
     otp = random.randint(1000,9999)
     return otp
 
-
-
 def captcha():
-    def rcs(x): # rcs for random character selector
-        return (x[random.randint(0,len(x)-1)])
-    
     length = int(input("Enter length of captcha: "))
-    while length <4:
+    while length <= 4:
         length = int(input("Re-enter captcha length (atleast 4): "))
-    sc = "!#$%&*?@^_"
-    uc = "QWERTYUIOPASDFGHJKLZXCVBNM"
-    lc = "qwertyuiopasdfghjklzxcvbnm"
-    digits = "1234567890"
+    
+    sc = ["!","#", "$", "%", "&", "*", "?", "@", "^", "_","/",]
+    uc = [chr(i) for i in range(65,91)]
+    lc = [chr(i) for i in range (97,123)]
+    digits = [str(i)for i in range(0,10)]
 
-    l = []
-    l += [rcs(sc)] + [rcs(uc)] + [rcs(lc)] + [rcs(digits)]
-    (random.shuffle(l))
     s = ''
-    for i in l:
+    t = [sc[random.randint(0,len(sc)-1)]]
+    t += [uc[random.randint(0,len(uc)-1)]]
+    t += [lc[random.randint(0,len(lc)-1)]]
+    t += [digits[random.randint(0,len(digits)-1)]]
+
+    random.shuffle(t)
+    for i in t:
         s += i
+    
+    l = sc + uc + lc + digits
+
     k = 4
-    total = sc + uc + lc + digits
     while k<=length:
-        s += rcs(total)
+        (random.shuffle(l))
+        s += l[0]
         k += 1
 
     return s
+
